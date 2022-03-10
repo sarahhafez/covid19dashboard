d5d174b6ded9f9b6b2aaea2f1af9a0ddbbf02acc
Sarah Hafez <shafez@Sarahs-MBP.cgocable.net>
2022-03-09 22:22:48 -0500

    added vacc status & case type graph + made changes to the time series graph design and code

--- server.R
+++ server.R
@@ -2,16 +2,28 @@ cases_link <-"https://health-infobase.canada.ca/src/data/covidLive/covid19-downl
 original_data_cases <- read.csv(url(cases_link)) %>% 
   mutate(date = lubridate::ymd(date))
 
+
+cases_and_vaccine_link <- "https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-casesAfterVaccination.csv"
+cases_and_vaccine_data <- read.csv(url(cases_and_vaccine_link)) %>%
+  select(-c("label.fr","prop_cases","prop_hospitalizations","prop_deaths"))  %>%
+  rename(Cases = num_cases,
+         Hospiatlization = num_hospitalizations,
+         Death = num_deaths) %>%
+  pivot_longer(cols=c("Cases","Hospiatlization","Death"),
+               names_to = "case_type" )
+
+
+
 # define function
 
 createdata <- function(province1,province2,province3) {
   if (province2 == "None" & province3 == "None") {
     filter_data <- original_data_cases %>% 
-      filter(prname == province1) 
+      dplyr::filter(prname == province1) 
     return(filter_data)
   } else if (province2 == "None" & province3 != "None") {
     filter_data <- original_data_cases %>% 
-      filter(prname %in% c(province1,province3))  
+      dplyr::filter(prname %in% c(province1,province3))  
     return(filter_data)
   } else if (province2 != "None" & province3 == "None") {
     filter_data <- original_data_cases %>% 
@@ -19,7 +31,7 @@ createdata <- function(province1,province2,province3) {
     return(filter_data)
   } else if (province2 != "None" & province3 != "None") {
     filter_data <- original_data_cases %>% 
-      filter(prname %in% c(province1,province2,province3))
+      dplyr::filter(prname %in% c(province1,province2,province3))
     return(filter_data)
   }
 }
@@ -29,7 +41,7 @@ function(input, output) {
   
   canada_latest_covid_numbers <- original_data_cases %>%
     arrange(date) %>%
-    filter(prname == "Canada") %>%
+    dplyr::filter(prname == "Canada") %>%
     tail(n=1)
   
   output$totalcases <- renderCountup({ 
@@ -60,7 +72,7 @@ function(input, output) {
       select(c("prname", "numdeathstoday", "numtoday")) %>%
       group_by(prname) %>%
       summarise("Total Deaths" = sum(numdeathstoday), "Total Cases" = sum(numtoday))  %>%
-      filter(prname!="Repatriated travellers" && prname!="Canada") %>%
+      dplyr::filter(prname!="Repatriated travellers" && prname!="Canada") %>%
       mutate(prname = replace(prname, prname == "Quebec", "Québec")) #needed for the map
     
     
@@ -76,95 +88,82 @@ function(input, output) {
       hc_title(text = "Total Cases by Province") %>%
       hc_subtitle(text = "Ontario and Quebec have recorded the highest number of cases") %>%
       hc_legend(enabled = F)
-      
+    
     
   })
   
   
-  output$time_series <- renderPlotly( {
+  output$time_series <- renderPlotly({
     
     if (input$time_series_data == "Daily Cases") {
-      our_plot <- createdata(input$province1,input$province2,input$province3) %>% 
-        ggplot(aes(date, numtoday,col=prname)) +
-        geom_line() + 
-        labs(title = "Number of Reported Cases per day",
-             x = "Date",
-             y = "Number of Reported Cases",
-             color="Province name")
-      plot <- our_plot +
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2020-12-14")))+
-        annotate("text", x=lubridate::ymd("2020-12-14"), y=0, 
-                 label="\nFirst dose starts")+
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2021-05-25")))+
-        annotate("text", x=lubridate::ymd("2021-05-25"), y=0, 
-                 label="\nSecond dose starts")+
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2021-11-12")))+
-        annotate("text", x=lubridate::ymd("2021-11-12"), y=0, 
-                 label="\nThird dose starts")
+      value = "numtoday"
+      ymax=max(createdata(input$province1,input$province2,input$province3)$numtoday)
+      t = "Number of Reported Cases per Day"
       
-      our_plotly_plot <- ggplotly(plot)
-      
-      return(our_plotly_plot)
-    } else if (input$time_series_data == "Daily Death") {
-      our_plot <- createdata(input$province1,input$province2,input$province3) %>%
-        ggplot(aes(date, numdeathstoday,col=prname)) +
-        geom_line() + 
-        labs(title = "Number of Death per day",
-             x = "Date",
-             y = "Number of Death",
-             color="Province name")
-      
-      plot <- our_plot +
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2020-12-14")))+
-        annotate("text", x=lubridate::ymd("2020-12-14"), y=0, 
-                 label="\nFirst dose starts")+
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2021-05-25")))+
-        annotate("text", x=lubridate::ymd("2021-05-25"), y=0, 
-                 label="\nSecond dose starts")+
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2021-11-12")))+
-        annotate("text", x=lubridate::ymd("2021-11-12"), y=0, 
-                 label="\nThird dose starts")
-      
-      our_plotly_plot <- ggplotly(plot)
-      
-      return(our_plotly_plot)
-    } else if (input$time_series_data == "Daily Active") {
-      our_plot <- createdata(input$province1,input$province2,input$province3) %>%
-        ggplot(aes(date, numactive,col=prname)) +
-        geom_line() + 
-        labs(title = "Number of Active Cases per day",
-             x = "Date",
-             y = "Number of Active Cases",
-             color="Province name")
-      
-      plot <- our_plot +
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2020-12-14")))+
-        annotate("text", x=lubridate::ymd("2020-12-14"), y=0, 
-                 label="\nFirst dose starts")+
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2021-05-25")))+
-        annotate("text", x=lubridate::ymd("2021-05-25"), y=0, 
-                 label="\nSecond dose starts")+
-        geom_vline(xintercept=as.numeric(lubridate::ymd("2021-11-12")))+
-        annotate("text", x=lubridate::ymd("2021-11-12"), y=0, 
-                 label="\nThird dose starts")
-      
-      our_plotly_plot <- ggplotly(plot)
-      
-      return(our_plotly_plot)
+    } else {
+      value = "numdeathstoday"
+      ymax=max(createdata(input$province1,input$province2,input$province3)$numdeathstoday)
+      t = "Number of Reported Deaths per Day"
     }
+    
+    
+    plot <- createdata(input$province1,input$province2,input$province3) %>% 
+      ggplot(aes_string(x="date", y=value,col="prname")) +
+      geom_line() + 
+      labs(title = t,
+           x = "Date",
+           y = "Number of Reported Cases",
+           color="Province") +
+      geom_vline(xintercept=as.numeric(lubridate::ymd("2020-12-14")), colour="grey", linetype="dotted")+
+      geom_text(aes(x=lubridate::ymd("2020-12-14"), y=ymax, 
+               label="First Dose"), colour="grey")+
+      geom_vline(xintercept=as.numeric(lubridate::ymd("2021-05-25")), colour="grey" , linetype="dotted")+
+      geom_text(aes(x=lubridate::ymd("2021-05-25"), y=ymax, 
+                    label="Second Dose"), colour="grey")+
+      geom_vline(xintercept=as.numeric(lubridate::ymd("2021-11-12")), colour="grey", linetype="dotted")+
+      geom_text(aes(x=lubridate::ymd("2021-11-12"), y=ymax, 
+                    label="Third Dose"), colour="grey")+
+      scale_color_manual(values = c("#4E84C4", "#E7B800", "#FC4E07"))+
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+            panel.background = element_blank(), axis.line = element_line(colour = "black"))
+    
+    our_plotly_plot <- ggplotly(plot)
+    
+    return(our_plotly_plot)
+    
+    
     
   }) 
   
-  
-  output$pieChart <-  renderHighchart({
-    link <-"https://health-infobase.canada.ca/src/data/covidLive/covid19-epiSummary-casesAfterVaccinationGender.csv"
-    df <- read.csv(url(link)) %>% select(-"label.fr")
+  output$cases_and_vaccines <- renderPlotly({
+    plot <- cases_and_vaccine_data %>%
+      dplyr::filter(case_type %in% input$casetype) %>%
+      ggplot(aes(x=case_type, y=value, fill = label.en)) +
+      geom_bar(position = "fill", stat = "identity") 
     
-    df %<>% filter(gender == input$gender) %>% select(-"gender")
-    df %>%
-      hchart("pie", hcaes(x = "label.en", y = "num_cases"),
-             name = "Number of Cases")
+    return(plot)
     
   })
+  
+  
+  output$cases_and_vaccines <- renderPlotly({
+    plot <- cases_and_vaccine_data %>%
+      filter(case_type %in% input$casetype) %>%
+      ggplot(aes(x=case_type, y=value, fill = label.en), label = scales::percent(value)) +
+      geom_bar( stat = "identity", position = "fill", width=0.3)  +
+      theme_classic() +
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+            panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+      scale_fill_manual(values = c("#293352", "#4E84C4",  "#E7B800", "#F4EDCA","#D16103" )) +
+      labs(fill = "Vaccination Status",
+           x = "Case Type",
+           y="Percentage Breakdown",
+           title = "Breakdown of Cases by Vaccination Status") 
+    
+    plot
+    
+    
+  })
+
 }
 
--- ui.R
+++ ui.R
@@ -2,17 +2,21 @@ library(shiny)
 library(bslib)
 library(shinydashboard)
 library(highcharter)
+library(tidyverse)
 library(plotly)
 library(ggplot2)
-library(tidyverse)
 library(magrittr)
 library(readr)
 library(shinyjqui)
 library(countup)
 
+#loading data
+
+
 cases_link <-"https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv"
 original_data_cases <- read.csv(url(cases_link)) %>% 
-  mutate(date = lubridate::ymd(date))
+  mutate(date = lubridate::ymd(date)) %>% 
+  dplyr::filter(prname != "Repatriated travellers")
 
 
 fluidPage(
@@ -20,9 +24,9 @@ fluidPage(
   # Application title
   titlePanel(
     h1("COVID-19: Past and Present Impact", align = "center")
-    ),
+  ),
   
-  h2("The Human Impact of COVID since December 2020", align = "center"),
+  h3("The Human Impact of COVID since December 2020", align = "center"),
   
   
   #First start with the Map of Canada, showing the total deaths and cases in each provinces
@@ -48,45 +52,50 @@ fluidPage(
   
   
   br(),
-
   
-  h2("How have vaccines contributed?",  align = "center"),
+  
+  h3("How have vaccines contributed?",  align = "center"),
+  
+  h4("Effect of Vaccinations on Trends",  align = "center"),
   br(),
-  
-  h3("Effect of Vaccinations on Trends",  align = "center"),
-  
   sidebarLayout(
     sidebarPanel(selectInput(inputId = "time_series_data",
-                             label = "Wat kind of number are you interested in?", 
-                             selected = "Cases", choices = c("Daily Cases", "Daily Death", "Daily Active")),
+                             label = "What kind of statistic are you interested in?", 
+                             selected = "Cases",
+                             choices = c("Daily Cases", "Daily Death")),
                  selectInput(inputId = "province1",
-                             label = "Which province are you interested in?", 
+                             label = "Which province are you interested in? \n (Choose up to 3)", 
                              selected = "Canada", choices = unique(original_data_cases$prname)),
                  selectInput(inputId = "province2",
-                             label = "Which province are you interested in?", 
+                             label = NULL, 
                              selected = "None", choices = c('None',unique(original_data_cases$prname))),
                  selectInput(inputId = "province3",
-                             label = "Which province are you interested in?", 
+                             label = NULL, 
                              selected = "None", choices = c('None',unique(original_data_cases$prname)))
     ),
     
     mainPanel(plotlyOutput("time_series"))
   ),
   
+  br(),
   
-  
-  
-  
-  h3("Breakdown of Vaccination Status by Cases Status and Gender",  align = "center"),
+  h4("Breakdown of Vaccination Status by Case Type",  align = "center"),
+  br(),
   
   verticalLayout(
-    highchartOutput("pieChart"),
-    radioButtons("gender",
-                 "Gender",
-                 choiceNames = c("Male", "Female", "All"),
-                 choiceValues = c("male", "female", "all")),
+    column(width=12,checkboxGroupInput( "casetype",
+                                        "Case Type (Check all applicable):",
+                                        choices= c("Cases", "Hospiatlization", "Death"),
+                                        inline = T,
+                                        selected = "Cases"),
+           align = "center"),
     
-  )
+    column(width=8, offset = 2, plotlyOutput("cases_and_vaccines")),
+    
+  ),
+  
+  br(),
+  
   
   
 )
